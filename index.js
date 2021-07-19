fea821a285d21ec195ea4daced2a92b823e9d0bf
Nick679-bee <mwauranicholas10@gmail.com>
2021-07-19 15:27:23 +0300

    project

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 let maleArray = ["Kwasi" , "Kwadwo" , "Kwabena" , "Kwaku" , "Yaw" , "Kofi" , "Kwame"];
-let femaleArray = ["Akosua" , "Adowoa" , "Abenaa" , "Akua" , "Yaa" , "Afua" , "Ama"]
-let dayArray = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"]
-let CC, YY, MM, DD, d, dayvalue
+let femaleArray = ["Akosua" , "Adowoa" , "Abenaa" , "Akua" , "Yaa" , "Afua" , "Ama"];
+let dayArray = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"];
+let CC, YY, MM, DD, d, dayvalue;
 
 function validate() {
-    var genders = document.getElementsByName("gender");
-    if( document.enterform.year.value == "" || document.enterform.year.value.length !=4 || document.enterform.year.value >2100 || document.myForm.year.value <=1900) {
-       alert( "Not valid!" );document.enterform.year.focus() ;
-       return false;}
+    let genders = document.getElementById("male");
+    console.log(genders);
+    if( document.enterform.year.value == "" || document.enterform.year.value.length !=4 || document.enterform.year.value >2100 || document.enterform.year.value <=1900) {
+       alert( "Not valid!" );
+       document.enterform.year.focus();
+    }
 
        else if( document.enterform.month.value == "" || isNaN( document.enterform.month.value ) || 
        document.enterform.month.value.length != 2 || document.enterform.month.value > 12  || document.enterform.month.value <= 0){
           alert( "Not valid!" );
           document.enterform.month.focus() ;
-          return false;}
+        }
 
           else if( document.enterform.date.value == "" || isNaN( document.enterform.month.value ) || 
   document.enterform.month.value.length != 2|| document.enterform.date.value > 31 || document.enterform.date.value <= 0) {
      alert( "Not valid!" );
      document.enterform.day.focus() ;
-     return false;}
+     }
 
-     else if(genders[0].checked==false && genders[1].checked==false ) {
+     else if(genders.checked==false && genders.checked==false ) {
         alert("Must select one");
-        return false;}
+        }
 
         else{
-            return true ;}
-        }
+         
 
         function calculateDayvalue(){
             year = document.getElementById("year").value;
@@ -40,16 +41,15 @@ function validate() {
             return (Math.floor(d));}
 
             function getGender(){
-                var genders = document.getElementsByName("gender");
+                let genders = document.getElementsByName("gender");
                 if(genders[0].checked == true){
-                  var gender = "male";}
+                  let gender = "male";}
 
             else if(genders[1].checked == true){
-    var gender = "female";}
+    let gender = "female";}
                 
     else{
-        return false;}
-    }
+       
     switch(gender){
         case "male":
           if (dayValue == 1){
