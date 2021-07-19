d3165260bc509399020a093103b9ced41e2bb16d
Nick679-bee <mwauranicholas10@gmail.com>
2021-07-19 10:50:40 +0300

    project

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const maleArray = ["Kwasi" , "Kwadwo" , "Kwabena" , "Kwaku" , "Yaw" , "Kofi" , "Kwame"];
-const femaleArray = ["Akosua" , "Adowoa" , "Abenaa" , "Akua" , "Yaa" , "Afua" , "Ama"]
+let maleArray = ["Kwasi" , "Kwadwo" , "Kwabena" , "Kwaku" , "Yaw" , "Kofi" , "Kwame"];
+let femaleArray = ["Akosua" , "Adowoa" , "Abenaa" , "Akua" , "Yaa" , "Afua" , "Ama"]
+let dayArray = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"]
+let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+
+
+
 
